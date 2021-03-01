7742b6af68d4a4a2cb67291595d144ed68d82441
Dabin <dabinim09@gmail.com>
2021-03-01 13:41:56 +0900

    practice

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 
 from flask import Flask, render_template, jsonify
-from threadCrawling import crawling
 
 app = Flask(__name__)
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
