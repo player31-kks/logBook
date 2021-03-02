7601a21d0e37eed5327b43774e8f3806cb82262a
Dabin <dabinim09@gmail.com>
2021-03-03 08:59:13 +0900

    logBook

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def login_post():
          'email': username_receive,
          'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
