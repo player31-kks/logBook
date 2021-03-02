18796397241d0a7e1ef893dfff5946113d54ee1b
kks <keumks0331@gamil.com>
2021-03-02 13:12:31 +0900

    modify join

--- static/join.js
+++ static/join.js
@@ -9,10 +9,10 @@ const birthLabel = document.querySelector('.join__birth')
 const birthInput = birthLabel.querySelector('input')
 
 const emailLabel = document.querySelector('.join__email')
-const emailInput = emailLabel.querySelector('.input')
+const emailInput = emailLabel.querySelector('input')
 
 const pwLabel = document.querySelector('.join__password')
-const pwInput = pwLabel.querySelector('.input')
+const pwInput = pwLabel.querySelector('input')
 
 
 function isNameVaild(asValue) {
@@ -23,7 +23,7 @@ function isNameVaild(asValue) {
 }
 
 function isBirthVaild(asValue) {
-  const regExp = /^[0-9]{6}/;
+  const regExp = /^[0-9]{6}$/;
   return regExp.test(asValue)
 }
 
@@ -41,7 +41,7 @@ function isPasswordVaild(asValue) {
 
 joinBtn.addEventListener('click', () => {
 
-  console.log(nameInput, birthInput, emailInput, pwInput)
+  console.log(nameInput.value, birthInput.value, emailInput.value, pwInput.value)
 
   if (!isNameVaild()) {
     nameInput.textContent = ""
@@ -63,23 +63,23 @@ joinBtn.addEventListener('click', () => {
     alert("패스워드가 유효하지 않습니다.영문, 숫자는 1개 씩 무조건 포함, 일부 특수문자 사용 가능, 8-20자 길이")
     return
   }
-  // $.ajax({
-  //   type: "POST",
-  //   url: "/api/signup",
-  //   data: {
-  //     name: nameInput.value,
-  //     birth: birthInput.value,
-  //     email: emailInput.value,
-  //     pw: pwInput.value
-  //   },
-  //   success: function (response) {
-  //     if (response['result'] == true) {
-  //       window.location = "#"
-  //     } else {
-  //       alert("회원가입 오류")
-  //     }
-  //   }
-  // })
+  $.ajax({
+    type: "POST",
+    url: "/api/signup",
+    data: {
+      name: nameInput.value,
+      birth: birthInput.value,
+      email: emailInput.value,
+      pw: pwInput.value
+    },
+    success: function (response) {
+      if (response['result'] == true) {
+        window.location.href = "/"
+      } else {
+        alert("회원가입 오류")
+      }
+    }
+  })
   console.log("성공")
 })
 
